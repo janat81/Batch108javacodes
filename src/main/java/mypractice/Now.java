5cfc3507bc5ea54df83e813c48eadd667778baef
zhanat güneş <guneszhanat@gmail.com>
2022-11-30 14:48:06 +0300

    country abbreviations

---
+++ src/main/java/mypractice/Now.java
@@ -0,0 +1,11 @@
+package mypractice;
+
+public class Now {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
---
+++ src/main/java/mypractice/Question9.java
@@ -0,0 +1,30 @@
+package mypractice;
+
+import java.util.Scanner;
+
+public class Question9 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("enter a country name");
+
+        String country = input.nextLine();
+
+        switch (country.toLowerCase()){
+            case "england" :
+                System.out.println("EN");
+                break;
+            case "france" :
+                System.out.println("FR");
+                break;
+            case "kazakhstan" :
+                System.out.println("KZ");
+                break;
+            default:
+                System.out.println("unexpected name");
+
+        }
+    }
+}
