2c4c4116223fb75486f3518197f093460176c7fd
zhanat güneş <guneszhanat@gmail.com>
2022-12-01 10:11:53 +0300

    absolute value of a number

---
+++ src/main/java/mypractice/Question12.java
@@ -0,0 +1,10 @@
+package mypractice;
+
+public class Question12 {
+    public static void main(String[] args) {
+
+        int x = -9;
+        int abs = x>0?x:x*(-1);
+        System.out.println(abs);
+    }
+}
