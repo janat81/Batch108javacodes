2a454895ee91e88c9626b0f194a855d04231f33b
zhanat güneş <guneszhanat@gmail.com>
2022-12-01 10:20:22 +0300

    object rslt of different return types

---
+++ src/main/java/mypractice/Question13.java
@@ -0,0 +1,12 @@
+package mypractice;
+
+public class Question13 {
+    public static void main(String[] args) {
+
+        int m = -100;
+        int n = 200;
+
+        Object rslt = m>0&&n>0 ? m*n:"I do not know";
+        System.out.println(rslt);
+    }
+}
