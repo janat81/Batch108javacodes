1d8f496a97b864f3141d33f65d62d735d6ad19c6
zhanat güneş <guneszhanat@gmail.com>
2022-12-01 10:00:58 +0300

    min of 2 integers

---
+++ src/main/java/mypractice/Question11.java
@@ -0,0 +1,14 @@
+package mypractice;
+
+public class Question11 {
+    public static void main(String[] args) {
+
+       int a = 5;
+       int b = 10;
+
+       char rslt = a<b?'a':'b';
+        System.out.println(rslt);
+
+    }
+
+}
