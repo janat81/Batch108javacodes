379682b85f190c11ad5cb25e63823b7debe7b409
zhanat güneş <guneszhanat@gmail.com>
2022-12-22 21:12:47 +0300

    from 44 to 11

---
+++ src/main/java/mypractice_1/Question_36.java
@@ -0,0 +1,11 @@
+package mypractice_1;
+
+public class Question_36 {
+    public static void main(String[] args) {
+        // Type all integers from 44 to 11
+
+        for (int i=44; i>10; i--){
+            System.out.print(i+"  ");
+        }
+    }
+}
