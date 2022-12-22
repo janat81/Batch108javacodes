0aa25e6bee1fd833af52162aa63605e9bc13c309
zhanat güneş <guneszhanat@gmail.com>
2022-12-22 21:08:48 +0300

    sout from 11 to 44

---
+++ src/main/java/mypractice_1/Question_35.java
@@ -0,0 +1,12 @@
+package mypractice_1;
+
+public class Question_35 {
+    public static void main(String[] args) {
+        // Type all integers from 11 to 44 in the same line with a space between consecutive integers
+
+
+        for (int i=11; i<45; i++){
+            System.out.print(i+"   ");
+        }
+    }
+}
