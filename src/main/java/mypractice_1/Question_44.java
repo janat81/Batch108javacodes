1064698f328c0904518142494fd8b380b1794e4c
zhanat güneş <guneszhanat@gmail.com>
2022-12-29 10:14:12 +0300

    product from 3to6

---
+++ src/main/java/mypractice_1/Question_44.java
@@ -0,0 +1,12 @@
+package mypractice_1;
+
+public class Question_44 {
+    public static void main(String[] args) {
+       //Find the multiplication from 3 to 6
+        int product = 1;
+        for (int i=3; i<7; i++){
+            product = product*i;
+        }
+        System.out.println(product);
+    }
+}
