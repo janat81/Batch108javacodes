105b5a592a76d3ebd552cde20c2afff42db9b822
zhanat güneş <guneszhanat@gmail.com>
2023-01-03 10:04:33 +0300

    nested for loop

---
+++ src/main/java/mypractice_1/Question_49.java
@@ -0,0 +1,15 @@
+package mypractice_1;
+
+public class Question_49 {
+    public static void main(String[] args) {
+        //Type code to get the output like
+
+        for (int i =1; i<5;i++){
+            System.out.println("Week " + ":" + i);
+            for (int d =1; d<8; d++) {
+                System.out.println("    Day " + ":" + d);
+            }
+        }
+
+    }
+}
