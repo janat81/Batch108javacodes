fcbe54f156f64514f2fd36501e07bf3e03a1eb12
zhanat güneş <guneszhanat@gmail.com>
2023-01-03 12:00:33 +0300

    triangle

---
+++ src/main/java/mypractice_1/Question_51.java
@@ -0,0 +1,17 @@
+package mypractice_1;
+
+public class Question_51 {
+    public static void main(String[] args) {
+
+     // Type code to get output like triangle
+
+        for (int i=1; i<7;i++){
+
+            for (int k=1; k<=i; k++){
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+
+    }
+}
