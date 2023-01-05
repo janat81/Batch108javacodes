60922717327be4a11804d3246dad10444ca4df97
zhanat güneş <guneszhanat@gmail.com>
2023-01-05 11:21:09 +0300

    sum of integers

--- src/main/java/mypractice_1/Question_52.java
+++ src/main/java/mypractice_1/Question_52.java
@@ -4,18 +4,21 @@ public class Question_52 {
     public static void main(String[] args) {
         //Print odd integers from 12 to 67
 
-        for (int i=12;i<68;i++){
+        for (int i = 12; i < 68; i++) {
 
-            if (i%2!=0){
-                System.out.print(i+"   ");
+            if (i % 2 != 0) {
+                System.out.print(i + "   ");
             }
         }
 
-        int k=12;
-        while (k<68){
-            if (k%2!=0){
-                System.out.print(k+"   ");
+        System.out.println("----------");
+        int k = 12;
+        while (k < 68) {
+            if (k % 2 != 0) {
+                System.out.print(k + "-");
             }
+            k++;
         }
     }
 }
+
---
+++ src/main/java/mypractice_1/Question_53.java
@@ -0,0 +1,15 @@
+package mypractice_1;
+
+public class Question_53 {
+    public static void main(String[] args) {
+    //Find the sum of integers from 12 to 67
+        int sum=0;
+        int i=12;
+        while (i<68){
+            sum=sum+i;
+            i++;
+
+        }
+        System.out.println(sum);
+    }
+}
